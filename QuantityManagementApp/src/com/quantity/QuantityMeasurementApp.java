fa351c80e1203430ffeb0e8d4cd92651b58a3eae
rajmehta03 <mraj54131@gmail.com>
2026-04-24 09:43:05 +0530

    UC8completed

---
+++ QuantityManagementApp/src/com/quantity/QuantityMeasurementApp.java
@@ -0,0 +1,18 @@
+package com.quantity;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+        QuantityLength feet = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength inches = new QuantityLength(12.0, LengthUnit.INCH);
+
+        System.out.println("Equality: " + feet.equals(inches));
+
+        QuantityLength result1 = feet.add(inches, LengthUnit.FEET);
+        System.out.println("Add in FEET: " + result1);
+
+        QuantityLength result2 = feet.convertTo(LengthUnit.INCH);
+        System.out.println("Convert FEET to INCH: " + result2);
+    }
+}
