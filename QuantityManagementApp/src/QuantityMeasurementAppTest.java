b8e2450ee41f9016e5b54de0c3c64e5861beee7d
rajmehta03 <mraj54131@gmail.com>
2026-04-24 08:59:33 +0530

    UC1completed

---
+++ QuantityManagementApp/src/QuantityMeasurementAppTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityMeasurementAppTest {
+
+    @Test
+    void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f2), "1.0 ft should equal 1.0 ft");
+    }
+
+    @Test
+    void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(f1.equals(f2), "1.0 ft should not equal 2.0 ft");
+    }
+
+    @Test
+    void testEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(f1.equals(null), "Value should not be equal to null");
+    }
+
+    @Test
+    void testEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f1), "Object should be equal to itself");
+    }
+
+    @Test
+    void testEquality_NonNumericInput() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        String nonNumeric = "not a number";
+
+        assertFalse(f1.equals(nonNumeric), "Feet should not equal non-numeric object");
+    }
+}
